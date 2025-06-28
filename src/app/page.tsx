6440e13f0fe5895486352255534ac5174758879c
time1043 <1665434994@qq.com>
2025-06-28 19:21:55 +0800

    add link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <main>Front page</main>;
+  return (
+    <div>
+      <Link href="/editor">Editor</Link>
+    </div>
+  );
 }
